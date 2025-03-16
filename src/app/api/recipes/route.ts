2fddeec089d006f460bfa3d9cf103660c2e0582d
abdisalam02 <niwache12@gmail.com>
2025-03-16 19:26:23 +0100

    Delete route.ts

--- src/app/api/recipes/route.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Recipe,
-  RecipeWithIngredients,
-  RecipeIngredient,
-} from '../../../../lib/types'; // Corrected import path
-import { getNutritionalInfo } from '../../../../lib/existingNutrition'; // Corrected import path
-
-/**
- 
